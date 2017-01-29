c85f145cb0cdcf5f6850c961522c03d78515cda1
wusicheng <1656423700@qq.com>
2017-01-30 00:27:30 +0800

    17-01-30-01

--- src/main/java/com/wsc/web/InitController.java
+++ src/main/java/com/wsc/web/InitController.java
@@ -1,18 +1,18 @@
 package com.wsc.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wsc.pojo.Teacher;
 import com.wsc.service.inter.IPersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -24,6 +24,7 @@ public class InitController {
 
     @Autowired
     private IPersonService iPersonService;
+    private ObjectMapper mapper=new ObjectMapper();
 
     @RequestMapping(value = "/list",method = RequestMethod.GET)
     public String list(Model model){
@@ -32,9 +33,15 @@ public class InitController {
         return "list";
     }
 
-    @GET
-    @Consumes({"application/json"})
-    @Produces({"application/json"})
-    @Path("/list")
-    public Object
+    @RequestMapping(value = "/getTeacherList",method = RequestMethod.GET)
+    public @ResponseBody List<Teacher> getTeacher(){
+        List<Teacher> list=iPersonService.queryTeacherAll();
+        return list;
+    }
+
+    @RequestMapping(value = "{id}",method = RequestMethod.GET)
+    public @ResponseBody Teacher getTeacherById(@PathVariable  int id){
+        Teacher teacher=iPersonService.queryTeacherByTeacherId(id);
+        return teacher;
+    }
 }
